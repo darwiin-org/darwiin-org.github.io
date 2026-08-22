64a06fcc1c4524552fa612c013f6297eccea5679
darvi <darvii@null>
2026-08-22 14:13:42 -0400

    a

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development environment for darviin/puredarwin.github.io-remake ";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            git
+            cmake
+            gnumake
+          ];
+
+          buildInputs = with pkgs; [
+            tcl
+          ];
+            shellHook = ''
+            echo "Execute 'make' to build." 
+            '';
+        };
+      }
+    );
+}
